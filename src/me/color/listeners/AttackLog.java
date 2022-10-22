8726d22316aa5d3a780fe57fb62cd34e97808599
lol <faceit.hoku@gmail.com>
2022-10-22 17:53:28 +0200

    inv null fix

--- src/me/color/listeners/AttackLog.java
+++ src/me/color/listeners/AttackLog.java
@@ -2,6 +2,7 @@ package me.color.listeners;
 
 import me.color.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -34,11 +35,26 @@ public class AttackLog implements Listener {
            if(hasCooldown.contains(e.getPlayer()) && midcd){
                 if(e.getPlayer().getName() == p1name){
                     Bukkit.broadcastMessage(e.getPlayer().getName() + " left midfight");
+
+                    for(int i = 0; i < p1inv.getSize(); i++){
+                        if(p1inv.getItem(i) == null){
+                            continue;
+                        }
+                        Bukkit.getWorld(e.getPlayer().getWorld().getName()).dropItem(e.getPlayer().getLocation(), p1inv.getItem(i));
+                    }
                     p1inv.clear();
                 }
 
                 if(e.getPlayer().getName() == p2name){
                     Bukkit.broadcastMessage(e.getPlayer().getName() + " left midfight");
+
+                    for(int i = 0; i < p2inv.getSize(); i++){
+                        if(p2inv.getItem(i) == null){
+                            continue;
+                        }
+                        Bukkit.getWorld(e.getPlayer().getWorld().getName()).dropItem(e.getPlayer().getLocation(), p2inv.getItem(i));
+                    }
+
                     p2inv.clear();
                 }
 
